9f63be2753e2575862334899449f241acc5506b4
Ritesh Kumar <ritesharya9955@gmail.com>
2026-08-21 02:47:35 +0530

    string

--- Array/array.java
+++ Array/array.java
@@ -1,14 +1,56 @@
 
 
 
-
-
-
-
-
 public class array {
+   static void printString(String str){
+      int n = str.length();
+      for(int i = 0; i<=n; i++){
+         char ch  = str.charAt(i);
+         System.out.println(ch);
+      }
+   }
+
+
+   //----------->COUNT VOWLES I STRING
+   static int getVowelCount(String str){
+      int count = 0;
+      for(int i = 0; i<str.length(); i++){
+         char ch = str.charAt(i);
+         if(ch == 'a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ||  ch == 'A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
+            count++;
+         }
+      }
+
+      return  count;
+   }
+
+
+
+   // -------> Reverse the string
+
+   static String reverseString(String str){
+      string reverse = "";
+      int n = str.length();
+      for(int i=n-1; i>=0; i--){
+         char ch = str.charAt(i);
+         reverse = reverse + ch;
+      }
+
+      return reverse;
+   }
+
 
    public static void main(String[] args) {
+
+       String str  = "ritesh";
+      System.out.println(reverseString(str));
+
+
+
+
+
+
+
         // // decleration 
         // int arr[];
         // // allocation
@@ -207,27 +249,170 @@ public class array {
      //-------------------> multilplication of the 2D array
 
 
-     int arr[][] = {{1,2,3},{1,2,3}};
+   //   int arr[][] = {{1,2,3},{1,2,3}};
 
-     int ans = 1;
-     for(int i = 0; i<arr.length; i++){
-        for(int j =0; j<arr[i].length; j++){
-            int value  = arr[i][j];
+   //   int ans = 1;
+   //   for(int i = 0; i<arr.length; i++){
+   //      for(int j =0; j<arr[i].length; j++){
+   //          int value  = arr[i][j];
 
-            ans = ans * value;
-        }
+   //          ans = ans * value;
+       
 
-     }
+     
+   //-----------> sum of array
 
-     System.out.println(ans);
+   // int arr[] = {2,4,5,6,78};
+
+   // int n = arr.length;
+   // int sum = 0;
+
+   // for(int i = 0; i<arr.length; i++){
+
+   //  int value = arr[i];
+
+   //    sum = sum + value;
+   // }
+   
+   // System.out.println(sum);
+
+      // int arr[] = {2,4,5,6,78};
+
+      // int n = arr.length;
+
+      // int MaxValue = arr[0];
+
+      //   for(int i = 0; i<arr.length; i++){
+      //    if(arr[i]>MaxValue){
+
+      //       MaxValue = arr[i];
+      //    }
+      //   }
+
+      //   System.out.println(MaxValue);
+
+
+      //     int[] arr = {10, 20, 30, 40, 50};
+
+      //   int start = 0;
+      //   int end = arr.length - 1;
+
+      //   while (start < end) {
+
+      //       int temp = arr[start];
+      //       arr[start] = arr[end];
+      //       arr[end] = temp;
+
+      //       start++;
+      //       end--;
+      //   }
+
+      //   System.out.println("Reverse Array:");
+
+      //   for (int i = 0; i < arr.length; i++) {
+      //       System.out.print(arr[i] + " ");
+      //   }
+
+      //----------------------> Liner search
+
+   //  int[] arr = {10, 20, 30, 40, 50};
+
+   //      int target = 30;
+
+   //      for (int i = 0; i < arr.length; i++) {
+
+   //          if (arr[i] == target) {
+   //              System.out.println("Element found at index: " + i);
+   //              return;
+   //          }
+   //      }
+
+   //      System.out.println("Element not found");
+
+   //-----------------> 2D Array Traversing
+
+   //  int arr[][] ={{1,2,3},{4,5,6}};
+      
+   //     for(int i = 0; i<arr.length; i++){
+   //       for(int j = 0; j<arr[i].length; j++){
+
+   //       System.out.print(arr[i][j] + "  ");
+
+   //       }
+   //       System.out.println();
+   //     }
+        
+   //  Scanner sc = new Scanner(System.in);
+   //  System.out.println("Provide  the strnf content: ");
+   //  String str = sc.nextLine();
+   //  System.out.println("vlue " + str );
+    
+   //----------------> Some method of string
+
+   // String  str = "Ritesh";
+   // System.out.println(str);
+   // System.out.println(str.charAt(2));
+   // System.out.println("");
+
+   //  String name = "RITESH";
+   //  System.out.println(str.equals(name));
+   //  System.out.println(str.equals(name));
+
+   
+    //empty -> length = 0
+    //blanks-> empty or sirf spaces h string
+   //   String str = " ";
+   //   System.out.println(str.length());
+   //   System.out.println(str.isEmpty());
+   //   System.out.println(str.isBlank());
+
+   //   String name = "  love  ";
+   //   System.out.println(name.length());
+   //  name =  name.trim();
+   //  System.out.println(name.length());
+
+   // String name = "ritesh";
+   // System.out.println(name.toUpperCase());
+   // System.out.println(name.toLowerCase());
+
+   // String name = " my name is ritesh ";
+   
+   //0 -> m
+   //1 -> y
+   //2 space and so on
+   // //begin index -> 3 
+   //  System.out.println(name.substring(3,7));\
+
+   // int num = 23445;
+   // String str = String.valueOf(num);
+   // System.out.println(num +1);
+   // System.out.println(str +1);
+
+   // String name = " my name is ritesh ";
+   // System.out.println(name.startsWith("my"));
+   // System.out.println(name.endsWith("ritesh"));
+
+   // String input = "my, name, is, love, ritesh";
+   // String[] words = input.split(",");
+      
+   // for(String str: words){
+   //    System.out.println(str);
+
+   }
+
+    
 
 
 
 
 
-        }
+     
+    }
+
+    
+        
 
         
-   }
+   
     
 
